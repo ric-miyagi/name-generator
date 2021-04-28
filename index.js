f05525edc1b23e6933601bb9afecd784d0e97475
Miyagi <ricardo2396andrade@hotmail.com>
2021-04-28 17:56:34 +0100

    Generates names and house at random

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const data = require("./data/database.json")
 
 function generateName(data) {
-    return "Complete me"
+    const randomFirstName = data.firstNames[generateRandomIndex(data.firstNames.length)];
+    return randomFirstName
 }
 
+
+function generateLast(data) {
+    const randomLastName = data.lastNames[generateRandomIndex(data.lastNames.length)];
+    return randomLastName
+}
+
+
+function generateHouse(data) {
+    const randomHouse = data.houses[generateRandomIndex(data.houses.length)];
+    return randomHouse
+}
+
+function generateRandomIndex(arrayLength) {
+    return Math.floor(Math.random() * arrayLength)
+}
+
+/*
 console.log(generateName(data))
+console.log(generateLast(data))
+console.log(generateHouse(data))
+*/
+
+console.log(`${generateName(data)} ${generateLast(data)} of House ${generateHouse(data)}`)
